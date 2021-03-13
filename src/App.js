bdfab32d8241f50cb304402808027447ede4bcc1
John <jdominguez2134@gmail.com>
2021-03-13 20:06:58 +0000

    updatedcard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React from 'react';
+import Squirrel from './Components/Squirrel.js';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        squirrels: []
+    }
   }
   
   // add a componentDidMount lifecycle method to fetch data from the API
+  componentDidMount = () => {
+      let appToken = 'esu1QrBEdWYvVrQ0KsIRFhfm5'
+      let query = `https://data.cityofnewyork.us/resource/vfnx-vebw.json?$$app_token=${appToken}&running=true`
+      fetch(query)
+        .then(response => response.json())
+        .then(data => {
+             this.setState(state => {
+            state.squirrels = data
+            return state
+     })
+        })
+        .catch(e => console.log(e))
+  }
 
   render() {
     return (
       <div className="App">
         <h1>2018 NYC Squirrel Survey Data</h1>
+        <h2>Number of Squirrels in Dataset: {this.state.squirrels.length}</h2>
+        {this.state.squirrels.map(squirrel => {
+            return <Squirrel name = {squirrel.unique_squirrel_id}
+                             age = {squirrel.age} 
+                             color = {squirrel.primary_fur_color}
+                             running = {squirrel.running}
+                             climbing = {squirrel.climbing}
+                             eating = {squirrel.eating}
+                             foraging = {squirrel.foraging}
+                             />
+        })}
       </div>
     );
   }
--- src/Components/Squirrel.js
+++ src/Components/Squirrel.js
@@ -4,6 +4,13 @@ function Squirrel(props) {
   return (
     <div className="Squirrel">
       <h3>SQUIRREL CARD!</h3>
+      <p>Name:{props.name}</p>
+      <p>Age: {props.age === "Adult" ? "I'm Old!" : "🍼"}</p>
+      <p>Color {props.color}</p>
+      <p> I'm running!!! {props.running ? "🏃": "No" }</p>
+      <p> Climbing? {props.climbing ? "I'm Climbing!" : "I'm on the ground"}</p>
+      <p>Eating? {props.eating ? "🍔" : "I'm not hungry!"}</p>
+      <p> Foraging?{props.foraging ? "😮" : ""}</p>
     </div>
   );
 }
